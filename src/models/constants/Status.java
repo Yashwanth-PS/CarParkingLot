818bf568d1f7b2c11bc2abedf24a68852536d91d
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2023-12-10 20:58:09 +0530

    CarParkingLot - Created Status Enum

---
+++ src/models/constants/Status.java
@@ -0,0 +1,12 @@
+package models.constants;
+
+public enum Status {
+    ACTIVE,
+    INACTIVE,
+    SUCCESSFUL,
+    PENDING,
+    FAILURE,
+    AVAILABLE,
+    NOT_AVAILABLE,
+    UNDER_MAINTENANCE;
+}
